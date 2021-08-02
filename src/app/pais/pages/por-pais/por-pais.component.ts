062aef2b2e87d8f2898210504711f840954cc8dc
Sebastian Asis <sasisromero10@gmail.com>
2021-08-02 13:47:07 -0500

    :dart: IMPROVE: http service

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = ''
+  hayError: boolean = false;
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) {   }
+  buscar(){
+    this.hayError = false;
+    this.paisService.buscarPais(this.termino).subscribe( (res)=> {
+      console.log(res)
+      this.paises=[...res]
+    }, (err)=>{
+      this.hayError = true;
+      this.paises = []
+      console.log(err)
+    })
+  }
+}
