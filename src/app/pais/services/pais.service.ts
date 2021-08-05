ddab555ae9ddc55d4c019069103af781592417e3
Sebastian Asis <sasisromero10@gmail.com>
2021-08-05 17:49:47 -0500

    :dart: IMPROVE: search sugester

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -8,15 +8,24 @@ import { Country } from '../interfaces/pais.interface';
 })
 export class PaisService {
   private apiUrl: string = 'https://restcountries.eu/rest/v2'
+  get getParams (){
+    return new HttpParams().set('field','name;capital;alpha2Code;flag;population');
+  }
   constructor(private http: HttpClient){ }
 
   buscarPais( termino: string): Observable<Country[]>{
-    const url = `${this.apiUrl}/name/${termino}`
+    const url = `${this.apiUrl}/name/${termino}?fields=name;capital;alpha2Code;flag;population`
     return this.http.get<Country[]>(url);
   }
 
   getPaisPorAlpha( id:string):Observable<Country>{
     const url = `${this.apiUrl}/alpha/${id}`
     return this.http.get<Country>(url);
+  }
+
+  getPaisesporRegion( region:string):Observable<Country[]>{
+
+    const url = `${this.apiUrl}/region/${region}`
+    return this.http.get<Country[]>(url,{params:this.getParams});
   }
 }
